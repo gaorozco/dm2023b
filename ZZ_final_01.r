d0c1ec9c761527aeab3e09e7c06c46f6b446a153
gaorozco <gorozco@itba.edu.ar>
2023-09-30 00:20:29 +0000

    auto commit 1       20230930 002028

--- ZZ_final_01.r
+++ ZZ_final_01.r
@@ -43,7 +43,7 @@ PARAM$exp_input <- "HT6510"
 PARAM$modelos_rank <- c(1)
 
 # reemplazar por las propias semillas
-PARAM$semillas <- c(102191, 200177, 410551, 552581, 892237)
+PARAM$semillas <- c(100019, 100043, 100049, 100057, 100069)
 
 PARAM$kaggle$envios_desde <- 9500L
 PARAM$kaggle$envios_hasta <- 11500L
