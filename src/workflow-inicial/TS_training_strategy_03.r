e87805b5a9edbc6e406af2e9ebb79c5a51dbfdb5
gaorozco <gorozco@itba.edu.ar>
2023-10-04 10:32:20 +0000

    auto commit 1       20231004 103218

--- src/workflow-inicial/TS_training_strategy_03.r
+++ src/workflow-inicial/TS_training_strategy_03.r
@@ -20,9 +20,9 @@ PARAM$exp_input <- "FE6310"
 
 PARAM$future <- c(202109)
 PARAM$final_train <- c(
-  202107, 202106, 202105, 202104, 202103, 202102,
+  202106, 202105, 202104, 202103, 202102,
   202101, 202012, 202011, 202010, 202009, 202008, 202002, 202001, 201912,
-  201911, 201910, 201909
+  201911, 201910, 201909, 201908
 )
 
 PARAM$train$training <- c(
@@ -30,8 +30,8 @@ PARAM$train$training <- c(
   201911, 201910, 201909, 201908, 201907, 201906, 201905, 201904, 201903
 )
 
-PARAM$train$validation <- c(202106)
-PARAM$train$testing <- c(202107)
+PARAM$train$validation <- c(202103, 202102)
+PARAM$train$testing <- c(202106, 202105, 202104)
 
 # Atencion  0.1  de  undersampling de la clase mayoritaria,  los CONTINUA
 # 1.0 significa NO undersampling ,  0.1  es quedarse con el 10% de los CONTINUA
